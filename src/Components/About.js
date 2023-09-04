a4e0693216c2b79ba113fa46e2085ac0fd418e09
Kalpak43 <111725804+Kalpak43@users.noreply.github.com>
2023-09-04 12:39:11 +0530

    Minor changes to about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,9 +4,9 @@ import advisor2 from "../Images/pranay-saraf.webp"
 const About = () => {
   return (
     <div className="About bg-red-500 md:px-52 px-5 py-4 text-justify text-gray-100">
-      <h1 className="text-4xl text-center font-extrabold mb-9">About Us</h1>
+      <h1 className="text-4xl text-center font-extrabold mb-9" data-aos="zoom-in">About Us</h1>
       <ul>
-        <li>
+        <li data-aos="fade-in">
           GDSC GHRCE is a community of students passionate about technology and
           development. It is a student-led initiative supported by Google
           Developers, aimed at providing a platform for students to learn,
@@ -18,7 +18,7 @@ const About = () => {
           platform where you can turn your ideas into reality.
         </li>
         <br />
-        <li>
+        <li data-aos="fade-in">
           At GDSC, we organize a diverse range of events and activities to cater
           to the interests of our members. From workshops on cutting-edge
           technologies like machine learning, cloud computing, and mobile app
@@ -30,13 +30,13 @@ const About = () => {
       </ul>
       <br />
       <h1 className="text-3xl text-center font-bold mb-9">Chapter Advisors</h1>
-      <div className="flex flex-wrap justify-evenly">
-        <div className="advisor-card max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
+      <div className="flex flex-wrap justify-evenly" >
+        <div className="advisor-card max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2" data-aos="fade-right">
           <div className="h-full flex flex-col justify-center items-center pb-2">
             <img
               className="w-24 h-24 mb-3 rounded-full shadow-lg"
               src={advisor1}
-              alt="Bonnie image"
+              alt="Nekita Chavan Mam"
             />
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
               Prof. Nekita Chavan Morris
@@ -46,12 +46,12 @@ const About = () => {
             </span>
           </div>
         </div>
-        <div className="advisor-card max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
+        <div className="advisor-card max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2" data-aos="fade-left">
           <div className="h-full flex flex-col justify-center items-center pb-2">
             <img
               className="w-24 h-24 mb-3 rounded-full shadow-lg"
               src={advisor2}
-              alt="Bonnie image"
+              alt="Pranay Saraf Sir"
             />
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
               Dr. Pranay Saraf
